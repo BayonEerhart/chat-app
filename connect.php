b239fcb7aa912d31f69c49466c39f6796a757e0d
Bayon <bayoneerhart43@gmail.com>
2023-06-08 14:07:49 +0200

    done

--- connect.php
+++ connect.php
@@ -2,8 +2,8 @@
 
 $host = '127.0.0.1';
 $db = 'chatapp';
-$user = 'bit_academy';
-$pass = 'bit_academy';
+$user = 'chat';
+$pass = '123';
 $charset = 'utf8mb4';
 $dsn = "mysql:host=$host;dbname=$db;charset=$charset";
 $pdo = new PDO($dsn, $user, $pass);
