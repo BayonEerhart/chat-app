4c85c66b8ad15fa8e62b8915564142281edcc70d
Bayon <bayoneerhart43@gmail.com>
2023-06-09 00:44:48 +0200

    done

--- check-updates.php
+++ check-updates.php
@@ -15,15 +15,13 @@ foreach ($rows as $row) {
         ($row["sender_id"] == $chater_id && $row["recipient_id"] == $user_id)
     ) {
         if ($row["id"] > $old_id) {
-            echo json_encode(true);
+            $list = [$row["content"], $row["sender_id"]];
+            echo json_encode($list);
             die();
         }
+        echo json_encode(false);
         die();
     }
 }
 ?>
-<script>
 
-'check-updates.php?id=<?php if (isset($highest_id)) {echo $highest_id;} ?>&chater_id=<?php if (isset($chater_id)) { $chater_id;} ?>&user_id=<?= $user_id ?>',
-
-</script>
